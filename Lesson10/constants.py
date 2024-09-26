fd407d94c42624ea7ffc7ed28e85fe12a1f2cf5e
valentinaandreevaa <sokolovavalentina579@gmail.com>
2024-09-26 22:52:08 +0300

    Lesson10

---
+++ Lesson10/constants.py
@@ -0,0 +1,7 @@
+# Первое задание
+Test_form_URL = "https://bonigarcia.dev/selenium-webdriver-java/data-types.html"
+# Второе задание
+Calculatot_URL = "https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html"
+# Третье задание
+Shop_URL = "https://www.saucedemo.com/"
+X_client_URL = " https://x-clients-be.onrender.com"
